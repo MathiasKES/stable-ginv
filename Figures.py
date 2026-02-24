08cec01c6200e62a47c2c766ddbb961d1e693bb0
Mathias from HPC <s234843@dtu.dk>
2026-02-24 14:30:36 +0100

    Should not be tabbed in

--- Figures.py
+++ Figures.py
@@ -47,9 +47,9 @@ def save_recon_panel(params: dict, panel_gt_pil, panel_idlg_pil, panel_masked_pi
         ax.imshow(panel_masked_pil[j], cmap='gray' if dataset == 'MNIST' else None)
         ax.axis('off')
 
-        # Leave space on the left for row labels
-        plt.tight_layout(rect=(0.08, 0.0, 1.0, 1.0))
-        out_path = os.path.join(save_dir, f"{'_'.join(f'{key}{val}' for key, val in params.items())}_{mask_desc}_{timestamp_str}.png")
-        plt.savefig(out_path, dpi=250, bbox_inches='tight')
-        plt.close(fig)
-        print("Saved 10-exp reconstruction panel to:", out_path)
+    # Leave space on the left for row labels
+    plt.tight_layout(rect=(0.08, 0.0, 1.0, 1.0))
+    out_path = os.path.join(save_dir, f"{'_'.join(f'{key}{val}' for key, val in params.items())}_{mask_desc}_{timestamp_str}.png")
+    plt.savefig(out_path, dpi=250, bbox_inches='tight')
+    plt.close(fig)
+    print("Saved 10-exp reconstruction panel to:", out_path)
