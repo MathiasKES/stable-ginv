5ec62e4638882b8db24dae0e103b3488cdf2dc3f
MathiasKES <warlour@eksworld.dk>
2026-06-03 11:56:09 +0200

    Remove no-op del imgs, labs in _Dataset_from_Image
    The objects remain referenced via self.imgs/self.labs, so deleting the
    local names had no effect.

--- functions/Dataset.py
+++ functions/Dataset.py
@@ -27,7 +27,6 @@ class _Dataset_from_Image(Dataset):
         self.imgs = imgs # img paths
         self.labs = labs # labs is ndarray
         self.transform = transform
-        del imgs, labs
 
     def __len__(self):
         return self.labs.shape[0]
